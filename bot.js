5378dd95f58efe0c47565b8a930dc9ba5f59d870
ElectronPro <71539127+ElectronPro@users.noreply.github.com>
2021-02-10 07:32:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,25 +11,25 @@ client.on('ready', () =>{
 client.on('message', msg=>{
   
   
-if(msg.content === "fake laugh"){
-  msg.channel.send('https://tenor.com/view/samay-raina-fake-laugh-comicstaan-samay-raina-laugh-samay-raina-fake-laugh-gif-18576907')
-}
+  if(msg.content ===  "fake laugh"){
+    msg.channel.send('https://tenor.com/view/samay-raina-fake-laugh-comicstaan-samay-raina-laugh-samay-raina-fake-laugh-gif-18576907')
+  }
   
-if(msg.content === "kuch bhi"){
+  if(msg.content ===  "kuch bhi"){
   msg.channel.send('https://tenor.com/view/comicstaan-comicstaan-season2-amazon-prime-video-amazon-prime-video-in-comicstaan2-gif-14558803')
-}
+  }
   
-if(msg.content === "welcome"){
-  msg.channel.send('https://tenor.com/view/samay-samay-raina-witamin-d-chess-streamer-gif-18479173')
-}
+  if(msg.content ===  "welcome"){
+    msg.channel.send('https://tenor.com/view/samay-samay-raina-witamin-d-chess-streamer-gif-18479173')
+  }
   
-if(msg.content === "wait karo"){
-  msg.channel.send('https://tenor.com/view/comicstaan-comicstaan-season2-amazon-prime-video-amazon-prime-video-in-comicstaan2-gif-14558674')
-}
+  if(msg.content ===  "wait karo"){
+    msg.channel.send('https://tenor.com/view/comicstaan-comicstaan-season2-amazon-prime-video-amazon-prime-video-in-comicstaan2-gif-14558674')
+  }
  
-if(msg.content === "besharamo"){
-  msg.channel.send('https://tenor.com/view/samay-raina-comicstaan-standup-comedy-standup-indian-comedian-gif-17498964')
-}
+  if(msg.content ===  "besharamo"){
+    msg.channel.send('https://tenor.com/view/samay-raina-comicstaan-standup-comedy-standup-indian-comedian-gif-17498964')
+  }
   
 })
 
